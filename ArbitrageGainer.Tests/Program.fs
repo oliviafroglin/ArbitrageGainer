4a1fa2e4e52828ca67a22f58e03e9b6d23a96b58
yukewF2023 <yukew@andrew.cmu.edu>
2024-04-13 01:22:11 -0700

    update program.fs.

--- ArbitrageGainer.Tests/Program.fs
+++ ArbitrageGainer.Tests/Program.fs
@@ -1,4 +1,25 @@
-module Program =
+// Program.fs
+open System
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Newtonsoft.Json
+open IdentifyCrossTradedPairsInfra
 
-    [<EntryPoint>]
-    let main _ = 0
+let app =
+    choose [
+        path "/cross-traded-pairs" >=> GET >=> fun ctx ->
+            async {
+                let task = IdentifyCrossTradedPairsInfra.identifyCrossTradedPairs () |> Async.StartAsTask
+                task.Result |> Set.iter (fun (pair1, pair2) -> printfn "%s-%s" pair1 pair2)
+                // let! pairs = identifyCrossTradedPairs ()
+                let json = JsonConvert.SerializeObject(task.Result)
+                return! OK json ctx
+            }
+    ]
+
+[<EntryPoint>]
+let main argv =
+    startWebServer defaultConfig app
+    0 // Return an integer exit code
